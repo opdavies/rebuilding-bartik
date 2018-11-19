0414033359ac2a723a88b98236c096122ac6d753
Oliver Davies <oliver@oliverdavies.uk>
2018-11-19 20:11:53 +0000

    Static template with Tailwind

---
+++ src/components/WelcomeToDrupal.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="bg-blue-dark text-white pt-4">
+      <div class="container mx-auto px-4 relative">
+        <div class="text-sm flex justify-end">
+          <a href="#0" class="text-white no-underline hover:underline">Log in</a>
+        </div>
+
+        <div class="flex items-center">
+          <img src="img/logo.svg" alt="" class="mr-4">
+          <div class="text-2xl">
+            <a href="#0" class="text-white no-underline">Drupal {{ version }}</a>
+          </div>
+        </div>
+
+        <nav class="mt-4">
+          <a href="#0" class="inline-block text-sm no-underline text-black bg-white px-3 py-2 rounded-t-lg">Home</a>
+        </nav>
+      </div>
+    </div>
+
+    <div class="bg-white pt-3 pb-12">
+      <div class="container mx-auto px-4">
+        <div class="flex -mx-8 my-6">
+          <div class="w-1/4 flex-none px-6">
+            <div class="p-4" style="background-color: #f6f6f2">
+              <h2 class="font-serif font-normal text-base border-b border-solid border-grey-light mb-3">Search</h2>
+
+              <div>
+                <form action="#" class="flex">
+                  <input type="text" class="border border-solid border-grey p-2">
+                  <button type="submit" class="bg-grey-light px-3 rounded-full border-b border-solid border-grey-dark ml-2" style="background-color: #f0f0f0">
+                    <img src="img/loupe.svg" class="block">
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div class="w-auto px-6">
+            <div class="font-serif">
+              <h1 class="font-normal">Welcome to Drupal {{ version }}</h1>
+              <p>No front page content has been created yet.</p>
+              <p>Follow the <a href="#0" class="text-blue-dark hover:text-blue no-underline border-b border-blue border-dotted hover:bg-solid">User Guide</a> to start building your site.</p>
+            </div>
+
+            <div class="mt-10">
+              <a href="#0">
+                <img src="img/feed.svg" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="text-xs text-white">
+      <div class="container mx-auto px-4 pt-16 pb-4">
+        <div class="border-t border-solid border-grey-darkest pt-6 -mb-6">
+          <div class="mb-6">
+            <p><a href="#0" class="text-white no-underline border-b border-dotted border-white">Contact</a></p>
+          </div>
+
+          <div class="mb-6">
+            <p>Powered by <a href="#0" class="text-white no-underline border-b border-dotted border-white">Drupal</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    version: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
