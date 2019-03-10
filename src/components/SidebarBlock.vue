308210965e72a3cc68245899fe91ca33c77a2b27
Oliver Davies <oliver@oliverdavies.uk>
2019-03-10 00:31:37 +0000

    Change colour scale

--- src/components/SidebarBlock.vue
+++ src/components/SidebarBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-grey-lightest p-4 mb-4">
+  <div class="bg-grey-10 p-4 mb-4">
     <slot></slot>
   </div>
 </template>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="bg-blue-dark">
+    <div class="bg-blue-30">
       <div class="py-4 text-white">
         <div id="header" class="xl:max-w-6xl mx-auto px-4 relative">
           <div class="flex flex-col-reverse">
@@ -26,8 +26,8 @@
         <drupal-message type="status">
           <p>
             A Bartik clone, built with
-            <a href="https://vuejs.org" class="text-blue-dark hover:text-blue no-underline border-b border-dotted hover:border-solid border-blue-dark">Vue.js</a>
-            and <a href="https://tailwindcss.com" class="text-blue-dark hover:text-blue no-underline border-b border-dotted hover:border-solid border-blue-dark">Tailwind CSS</a>.
+            <a href="https://vuejs.org" class="text-blue-30 hover:text-blue-20 no-underline border-b border-dotted hover:border-solid border-blue-30">Vue.js</a>
+            and <a href="https://tailwindcss.com" class="text-blue-30 hover:text-blue-20 no-underline border-b border-dotted hover:border-solid border-blue-30">Tailwind CSS</a>.
           </p>
         </drupal-message>
 
@@ -48,13 +48,13 @@
 
           <div class="w-full md:w-1/3 lg:w-1/4 flex-none md:px-6 -mb-4">
             <sidebar-block>
-              <h2 class="font-serif font-normal text-base text-grey-darkest border-b border-solid border-grey-light mb-3">Search</h2>
+              <h2 class="font-serif font-normal text-base text-grey-60 border-b border-solid border-grey-30 mb-3">Search</h2>
 
               <div>
                 <form action="#" class="flex">
-                  <input type="text" class="border border-solid border-grey p-2 w-full xl:w-auto">
+                  <input type="text" class="border border-solid border-grey-40 p-2 w-full xl:w-auto">
 
-                  <button type="submit" class="bg-grey-lighter px-3 rounded-full border-b border-solid border-grey-dark ml-2 flex-none">
+                  <button type="submit" class="bg-grey-20 px-3 rounded-full border-b border-solid border-grey-50 ml-2 flex-none">
                     <img src="img/loupe.svg" class="block">
                   </button>
                 </form>
@@ -67,7 +67,7 @@
 
     <div id="footer" class="text-xs text-white">
       <div class="xl:max-w-6xl mx-auto px-4 pt-16 pb-4">
-        <div class="border-t border-solid border-grey-darkest pt-6 -mb-6">
+        <div class="border-t border-solid border-grey-60 pt-6 -mb-6">
           <div class="mb-6">
             <p><a href="#0">Contact</a></p>
           </div>
@@ -114,11 +114,11 @@ export default {
     @apply underline
 
 #main a
-  @apply text-blue-dark no-underline border-b border-blue border-dotted
+  @apply text-blue-30 no-underline border-b border-blue-10 border-dotted
 
   &:hover,
   &:focus
-    @apply text-blue border-solid
+    @apply text-blue-10 border-solid
 
 #footer a
   @apply text-white no-underline border-b border-dotted border-white
