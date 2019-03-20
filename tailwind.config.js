5853da96c388f19d1cd2e0613cfc9e4905dc999d
Oliver Davies <oliver@oliverdavies.uk>
2019-03-20 23:58:56 +0000

    Container is now a core plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,10 +37,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('tailwindcss/plugins/container')({
-      center: true,
-      padding: '1rem',
-    }),
     require('tailwindcss-skip-link')(),
   ]
 }
