e664fc3191b1c74f109361a04450ea24e23e9026
Oliver Davies <oliver@oliverdavies.uk>
2019-03-24 20:30:22 +0000

    Split config files

--- src/main.js
+++ src/main.js
@@ -6,5 +6,5 @@ Vue.config.productionTip = false
 Vue.component('drupal-message', require('@/components/DrupalMessage').default)
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
