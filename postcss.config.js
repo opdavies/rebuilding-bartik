4f4cd21956425e64e36a10e54f334438d7ab116c
Oliver Davies <oliver@oliverdavies.uk>
2019-08-12 01:56:21 +0100

    Add PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  parser: 'sugarss',
-  plugins: {
-    tailwindcss: './tailwind.config.js',
-    autoprefixer: {}
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.vue',
+        './public/index.html',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  ]
 }
+
