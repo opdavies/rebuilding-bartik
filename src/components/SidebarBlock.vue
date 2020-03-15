1359fa8159022b6b9dc05cc09113a1c513dd9aa1
Oliver Davies <oliver@oliverdavies.uk>
2020-03-15 23:57:12 +0000

    Remove Vue.js code
    References #34References #34

--- src/components/SidebarBlock.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="p-4 mb-4 bg-grey-100">
-    <slot></slot>
-  </div>
-</template>
--- src/components/SkipLink.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <a href="#main-content" class="skip-link py-1 px-2 rounded-b-lg text-white bg-black-60 focus:no-underline focus:outline-none">Skip to main content</a>
-  </div>
-</template>
